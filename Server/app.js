e2478bcd3f719185ebe7fb32a34ff4468d91b372
sydsan1801 <sydsan1801@gmail.com>
2024-11-10 16:49:29 +0530

    third commit

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,12 @@
 const express=require("express")
 const app=express()
 const cors=require("cors")
-// const corsOptions = {
-//   origin: 'http://localhost:5173',  // Your frontend URL
-//   methods: 'GET, POST, PUT, DELETE',  // Allow specific HTTP methods
-//   allowedHeaders: 'Content-Type, Authorization',  // Allow 'Authorization' header
-// };
 
-app.use(cors());  // Use the CORS middleware with the options
+app.use(cors(
+   { origin:['https://bookmark-bliss-frontend.vercel.app'] ,  
+  methods: ['GET, POST, PUT, DELETE'], 
+  credentials: true,  }
+));  
 app.use(express.json())
 
 require("dotenv").config()
